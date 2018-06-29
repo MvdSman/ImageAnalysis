926a512291ab69aaff6179546d2679dc660b254d
dru93 <33983402+dru93@users.noreply.github.com>
2018-06-29 13:29:39 +0200

    Add files via upload

--- code6.m
+++ code6.m
@@ -5,19 +5,11 @@
 
 % 6.1
 
-% Preallocation
-n_images = 58;
-[absolute_blue, relative_blue] = deal(zeros(1,n_images));
-[images, filt, masked, m] = deal(cell(1, n_images));
-[texture, measurements] = deal(zeros(n_images, 4));
-
-
 % Read files
 numfiles = 58;
 pf = 'w8t1t2_d7_';
 sf = '.tif';
 for i = 1:numfiles
-%     index = num2str(i);
     originals{i} = readim(strcat(pf, num2str(i), sf));
 end
 clearvars pf sf i;
@@ -26,12 +18,12 @@ clearvars pf sf i;
 
 for i = 1:numfiles
     % 6.1
-    filt{i} = threshold(originals{i}{3}, 'isodata', Inf);
-    filt{i} = bclosing(filt{i}, 2, -1, -1);
+    orig_filtered{i} = threshold(originals{i}{3}, 'isodata', Inf);
+    orig_filtered{i} = bclosing(orig_filtered{i}, 2, -1, -1);
     
     % 6.3
-    % Size and color measurements
-    % Column values of [measurements] matrix: 
+    % Size and color properties
+    % Column values of [properties] matrix: 
     % 1: Size; 
     % 2: Perimeter; 
     % 3: Blueness(absolute);  
@@ -40,30 +32,22 @@ for i = 1:numfiles
     % 6: Assigned class (K-means) based on shape/blueness
     % 7: Assigned class (K-means) based on texture
 
-    m{i} = measure(squeeze(filt{i}),[],{'Size','Perimeter','Feret','ConvexArea','P2A','PodczeckShapes','Convexity'},[],Inf,0,0);
-    [measurements(i,1), index] = max(m{i}.Size);
-    measurements(i,2) = max(m{i}.Perimeter);
-    R = measure(filt{i},originals{i}{1},{'Mean','Sum'},[],Inf,0,0);
-    G = measure(filt{i},originals{i}{2},{'Mean','Sum'},[],Inf,0,0);
-    B = measure(filt{i},originals{i}{3},{'Mean','Sum'},[],Inf,0,0);
-    measurements(i,3) = max(B.Sum);
-    measurements(i,4) = measurements(i,3)/(max(R.Sum) + max(G.Sum) + max(B.Sum));
-    measurements(i,5) = m{i}.PodczeckShapes(5,index);
+    m{i} = measure(squeeze(orig_filtered{i}),[],{'Size','Perimeter','Feret','ConvexArea','P2A','PodczeckShapes','Convexity'},[],Inf,0,0);
+    [properties(i,1), index] = max(m{i}.Size);
+    properties(i,2) = max(m{i}.Perimeter);
+    R = measure(orig_filtered{i},originals{i}{1},{'Mean','Sum'},[],Inf,0,0);
+    G = measure(orig_filtered{i},originals{i}{2},{'Mean','Sum'},[],Inf,0,0);
+    B = measure(orig_filtered{i},originals{i}{3},{'Mean','Sum'},[],Inf,0,0);
+    properties(i,3) = max(B.Sum);
+    properties(i,4) = properties(i,3)/(max(R.Sum) + max(G.Sum) + max(B.Sum));
+    properties(i,5) = m{i}.PodczeckShapes(5,index);
     
 end
-
-
-% Clustering
-species = kmeans(measurements, 3);
-species2 = kmeans(texture, 3);
-measurements = [measurements species];
-measurements = [measurements species2];
-
 
 % 6.2
 figure
 for i = 1:numfiles
     subplot(4,15,i); %
-    imshow(dip_array(filt{i})*255);
+    imshow(dip_array(orig_filtered{i})*255);
     drawnow
 end
